3b7dc198843d117a8b2e9d952d792c318bd9ad88
pratamb2 <matthew.prata@jhuapl.edu>
2026-01-31 16:24:17 -0500

    general make structure

--- homework1/src/main.c
+++ homework1/src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello World");
+int main(int argc, char *argv[]) {
+	printf("You have entered %d arguments (including program name):\n", argc);
+
+	// Loop through each argument from argv[0] to argv[argc - 1]
+	for (int i = 0; i < argc; i++) {
+		printf("Argument %d: %s\n", i, argv[i]);
+	}
+
 	return 0;
 }
