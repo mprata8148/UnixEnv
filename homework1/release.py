f2ffe29fb55e55b32fd0191eed5b43009572d29c
pratamb2 <matthew.prata@jhuapl.edu>
2026-02-01 11:43:41 -0500

    added python script to build tarbal

--- homework1/release.py
+++ homework1/release.py
@@ -1,7 +1,7 @@
 import sys
 import os
-
-
+import subprocess
+import re
 def confirm(type: str) -> int:
 	# Check if user wants to continue
 
@@ -13,20 +13,49 @@ def confirm(type: str) -> int:
 	print("Invalid input: exiting")
 	return 1
 
+def get_hostname() -> str:
+	# Get hostname
+	#Safe Characters allowed for name mainly to prevent // or \
+	SAFE_NAME = re.compile(r"^[A-Za-z0-9._-]+$")
+
+	hostname = input("Enter hostname: ").split()[0]
+	if not hostname:
+		raise ValueError("Hostname cannot be empty")
+	if not SAFE_NAME.match(hostname):
+		raise ValueError("Hostname contains unsafe characters")
+	return str(hostname)
+
 def build_binary():
+	tar_name = "homework1"
 	if confirm("binary release") == 1:
 		print("Exiting...")
 		return
 	print("Building binary release...")
-	pass
+	subprocess.call(["make", "clean"])
+	subprocess.call(["make", "all"])
+	hostname = get_hostname()
+	tar_name += "_" + hostname
+	#Generate TARBALL THAT INCLUDES a  tar -czf homework1_bin_contents.tar.gz -C homework1/bin contents
+	return_code = subprocess.call(["tar", "-czf", f"{tar_name}.tar", "-C",  ".." ,"homework1/bin"])
+	if return_code != 0:
+		print("Failed to create tarball")
+		return
+
+	print(f"Tarball created: {tar_name}.tar")
 
 def build_source():
+	tar_name = "homework1"
 	if confirm("source release") == 1:
 		print("Exiting...")
 		return
 	print("Building binary release...")
-	pass
-	pass
+	subprocess.call(["make", "clean"])
+	#Generate TARBALL THAT INCLUDES a  tar -czf homework1_bin_contents.tar.gz -C homework1/bin contents
+	return_code = subprocess.call(["tar", "-czf", f"{tar_name}.tar", "-C",  ".." ,"homework1"])
+	if return_code != 0:
+		print("Failed to create tarball")
+		return
+	print(f"Tarball created: {tar_name}.tar")
 
 def main(argv: list[str]):
 	# Check if second argument exists
